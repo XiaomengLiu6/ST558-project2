35935ab0d0672d964e173ef7aa1de56bdd66a23d
Xiaomeng Liu <l0x0m00402@gmail.com>
2023-10-09 11:57:37 -0400

    final commit

--- README.R
+++ README.R
@@ -1,3 +1,12 @@
-# READ ME
-# numerical summary
-# two plots
+
+# render the "new.RMD" file as a .md file called README.md.
+
+rmarkdown::render(
+  input = "new.rmd",
+  output_format = "github_document",
+  output_file = "README.md",
+  runtime = "static",
+  clean = TRUE,
+  params = NULL,
+  encoding = "UTF-8"
+)
